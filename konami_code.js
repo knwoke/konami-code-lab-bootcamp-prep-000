a046161fb75f25c26ba79f02d6bb9fc3d19eff75
Kevin Nwoke <kevinnwoke@yahoo.com>
2018-06-17 18:40:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,22 @@ const codes = [
 
 function init() {
   // your code here
-}
+  document.body.addEventListener('keydown',function(e){
+       var key = parseInt(e.which || e.detail);
+       var index=0;
+       if(key === codes[index]){
+         index++;
+
+          if(index === codes.length){
+            alert("You did it!");
+            index=0;
+      }
+   }
+      else{
+        console.log(e.which);
+        index=0;
+      }
+ })
+ }
+
+ init();
